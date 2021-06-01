f2a66f8b5fc0272898a2c1c87e0223235c846abc
kensei <kensei.kitahara@gmail.com>
2021-06-02 06:15:47 +0900

    add todo initial scene

---
+++ client/todo/Assets/Scripts/Controllers/TodoController.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TodoController : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject m_scrollContent;
+    [SerializeField]
+    private RectTransform m_scrollElement;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        for (int i = 0; i < 20; i++)
+        {
+            var listElement = Instantiate(m_scrollElement, Vector3.zero, Quaternion.identity);
+            listElement.transform.parent = m_scrollContent.transform;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
